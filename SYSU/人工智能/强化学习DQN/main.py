039b4cf1bfec60f0df52f8021e34da79061a5d70
X-Miranda <142312879+X-Miranda@users.noreply.github.com>
2025-10-13 11:25:54 +0800

    Add files via upload

---
+++ SYSU/人工智能/强化学习DQN/main.py
@@ -0,0 +1,46 @@
+import argparse
+import gym
+from argument import dqn_arguments, pg_arguments
+import matplotlib.pyplot as plt
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="SYSU_RL_HW2")
+    parser.add_argument('--train_pg', default=False, type=bool, help='whether train policy gradient')
+    parser.add_argument('--train_dqn', default=False, type=bool, help='whether train DQN')
+
+    # parser = dqn_arguments(parser)
+    parser = pg_arguments(parser)
+    args = parser.parse_args()
+    return args
+
+
+def run(args):
+    if args.train_pg:
+        env_name = args.env_name
+        env = gym.make(env_name)
+        from agent_dir.agent_pg import AgentPG
+        agent = AgentPG(env, args)
+        agent.run()
+
+    if args.train_dqn:
+        env_name = args.env_name
+        env = gym.make(env_name)
+        from agent_dir.agent_dqn import AgentDQN
+        agent = AgentDQN(env, args)
+        agent.run()
+
+def draw(total_rewards):
+    plt.figure(figsize=(10, 5))
+    plt.plot(total_rewards, label='Total Reward per Episode')
+    plt.xlabel('Episode')
+    plt.ylabel('Total Reward')
+    plt.title('Reward vs. Episode for DQN Agent')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    return
+
+if __name__ == '__main__':
+    args = parse()
+    run(args)
